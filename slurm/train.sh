15d9bd865ea34418bc5075496e78c95f52fe427f
Beilong Tang <btang1@login-01.ncshare.org>
2026-09-10 13:57:34 +0000

    update slurm

--- slurm/train.sh
+++ slurm/train.sh
@@ -14,8 +14,11 @@
 set -e 
 
 # monitor gpu usage
+mkdir -p gpu_usage
 nvidia-smi --query-gpu=timestamp,utilization.gpu,utilization.memory,memory.used,memory.total \
-    --format=csv -l 30 > gpu_usage.log &
+    --format=csv -l 30 > "gpu_usage/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.log" &
+NVSMI_PID=$!
+trap 'kill $NVSMI_PID 2>/dev/null' EXIT
 
 
 # 00 transfer the imagenet data to the scratch folder for faster loading
@@ -23,9 +26,11 @@ SRC="/work/btang1/data/Imagenet_process/imagenet_train"
 ./shell/00_copy_data.sh $SRC
 
 # 01 training 
+MEM_ALLOC=30
 torchrun --nnodes=1 --nproc_per_node=4 train.py --model DiT-XL/2 \
     --data-path /scratch/imagenet \
     --global-batch-size 256 \
     --vae "mse" \
     --epochs 80 \
-    --resume 
+    --resume \
+    --mem-alloc $MEM_ALLOC
